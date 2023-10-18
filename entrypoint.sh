27ef7e439d4557ad96b6a054faf8ff4bef624ffe
blupants <blupants.robot@gmail.com>
2023-10-18 15:26:17 -0500

    Fixed action argument index

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ while getopts "a:b:c:d:e:f:g:h:i:j:k:l:m:n:o:p:q:r:s:t:u:v:z:" o; do
        n)
          export debug=${OPTARG}
        ;;
-       i)
+       o)
          export reportFormat=${OPTARG}
        ;;
   esac
